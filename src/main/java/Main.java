29984d779ba9ae3d9dc731b2be7f01de62c17ab1
adamusek-007 <adam.kuczabinski@gmail.com>
2023-05-30 08:33:11 +0200

    Added chromedriver

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,18 @@
 package main.java;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriver webDriver = createWebDriver();
 
 	}
-
+	private static WebDriver createWebDriver() {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		return driver;
+	}
 }

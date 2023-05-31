78679172d04ba74159c2dcf6add6aad803b6e83e
adamusek-007 <adam.kuczabinski@gmail.com>
2023-05-31 15:47:55 +0200

    Made JobBoard class

---
+++ src/main/java/JobBoard.java
@@ -0,0 +1,27 @@
+package main.java;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class JobBoard {
+	public int id;
+	public String name = "";
+	public String link = "";
+	public List<String> instructionsToExecute;
+	
+	public JobBoard(int id, String name, String link, String instructions) {
+		this.id = id;
+		this.name = name;
+		this.link = link;
+		this.instructionsToExecute = createListOfInstructions(instructions);
+	}
+
+	private List<String> createListOfInstructions(String instructions) {
+		List<String> instructionsList = new ArrayList<>();
+		String[] arrayOfInstructions = instructions.split(";");
+		for (String instruction : arrayOfInstructions) {
+			instructionsList.add(instruction);
+		}
+		return instructionsList;
+	}
+}

56c531bb662891aaf66d54c009ffe9042f818bf2
adamusek-007 <adam.kuczabinski@gmail.com>
2023-05-31 11:54:28 +0200

    Deleted TODO tag

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,13 @@ package main.java;
 
 import java.util.Set;
 
-
 public class Main {
 
 	public static void main(String[] args) {
 		DatabaseOperator.checkIsDriverInstalled();
 		DatabaseOperator.setUpConnectionWithDatabase();
 		DatabaseOperator.getBoardsDataFromDatabase();
-		
 		Set<String> jobBoardsNames = DatabaseOperator.jobBoardsData.keySet();
-
 		for (String j : jobBoardsNames) {
 			System.out.println(j);
 		}
@@ -23,7 +20,6 @@ public class Main {
 //				String jobBoardName = companies.getString("job_board_name");
 //				String jobBoardLink = companies.getString("job_board_link");
 //				System.out.println("Skanowanie: " + jobBoardName);
-////				TODO
 ////				openWebsite(webDriver, jobBoardLink, "visible");
 ////				openWebsite(webDriver, jobBoardLink, "must-proceed-searching");
 //			}

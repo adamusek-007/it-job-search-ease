42c6acfe43f7c460506a139f9dd40bda90360764
adamusek-007 <adam.kuczabinski@gmail.com>
2023-06-02 23:37:06 +0200

    Added access modifiers to field's and set to private.

--- src/main/java/Job.java
+++ src/main/java/Job.java
@@ -1,40 +1,69 @@
 package main.java;
 
 public class Job {
-	String title;
-	String[] techStack;
-	String earnings;
-	String localisation;
-	String link;
-	String company;
-	String boardId;
+
+	private String title;
+	private String[] techStack;
+	private String earnings;
+	private String localisation;
+	private String link;
+	private String company;
+	private String boardId;
+
+	public String getJobTitle() {
+		return this.title;
+	}
+
+	public String[] getTechStack() {
+		return techStack;
+	}
+
+	public String getEarnings() {
+		return earnings;
+	}
+
+	public String getLocalisation() {
+		return localisation;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public String getBoardId() {
+		return boardId;
+	}
 
 	public void setJobTitle(String jobTitle) {
-		title = jobTitle;
+		this.title = jobTitle;
 	}
 
 	public void setJobTechStack(String[] arrayWithTechStack) {
-		techStack = arrayWithTechStack;
+		this.techStack = arrayWithTechStack;
 	}
 
 	public void setJobEarnings(String jobEarnings) {
-		earnings = jobEarnings;
+		this.earnings = jobEarnings;
 	}
 
 	public void setJobLocalisation(String jobLocal) {
-		localisation = jobLocal;
+		this.localisation = jobLocal;
 	}
 
 	public void setJobLink(String jobLink) {
-		link = jobLink;
+		this.link = jobLink;
 	}
 
 	public void setJobCompany(String jobCompany) {
-		company = jobCompany;
+		this.company = jobCompany;
 	}
 
 	public void setJobBoardId(String id) {
-		boardId = id;
+		this.boardId = id;
 	}
 
 }

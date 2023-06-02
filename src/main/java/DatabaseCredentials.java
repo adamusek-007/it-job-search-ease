5fc38c573fd9737478f94e31eea9f8fe0053ddd4
adamusek-007 <adam.kuczabinski@gmail.com>
2023-06-02 23:38:37 +0200

    Changed fields access modifiers

--- src/main/java/DatabaseCredentials.java
+++ src/main/java/DatabaseCredentials.java
@@ -5,8 +5,8 @@ public class DatabaseCredentials {
 	public static final String userName = "root";
 	public static final String userPassword = "";
 	public static final String dbName = "job_offers";
-	protected static String dbAddr = "localhost";
-	protected static String dbPort = "3306";
-	protected static final String dbURL = "jdbc:mysql://" + dbAddr + ":" + dbPort + "/" + dbName;
+	private static String dbAddr = "localhost";
+	private static String dbPort = "3306";
+	public static final String dbURL = "jdbc:mysql://" + dbAddr + ":" + dbPort + "/" + dbName;
 
 }
